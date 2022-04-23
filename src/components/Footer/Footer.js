e549da6fbaa2105d26acaa9c49fe5c9f78b703d0
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-23 21:05:31 +0600

    style footer area

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,32 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div>
-            <h1>Thsi is footer</h1>
-        </div>
+        <>
+            <div className='footer-area'>
+                <div className='footer'>
+                    <div className='icon'>
+                        <h1>mealDb</h1>
+                        <i class="fa-brands fa-facebook"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                    <div className="contact">
+                        <span>Enter Your Email</span> <br />
+                        <input className='footer-iinput' type="email" placeholder='Enter your email' /> <br />
+                        <button className='regural-btn'>submit</button>
+                    </div>
+                    <div className="location">
+                        <p>
+                            401 Khulna Sador <br />
+                            Dhaka, Bangladesh
+                        </p>
+                    </div>
+                </div>
+                <p>Copyright2022 &#169;</p>
+            </div>
+
+
+        </>
     );
 };
 
